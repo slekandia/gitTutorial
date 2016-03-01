e636a3dff34c75d1135108b213c9b8d184d73d2a
slekandia <metincalis1@gmail.com>
2016-03-01 18:52:49 +0200

    added the java files

---
+++ YoWorld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class YoWorld{
+	public int myNum;
+	public static void main(String[] args) {
+		if (args[0] != null)
+	      System.out.println("Yo World" + args[0]);
+	    else
+		  System.out.println("Yo World");
+
+	 }
+}
